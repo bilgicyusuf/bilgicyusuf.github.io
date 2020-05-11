89d19ac9dcec5265a6daeee68ef2d1d697d6da60
Celal Savur <cs1323@rit.edu>
2020-05-11 05:05:32 -0400

    version 1.1

--- _site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "UPSTAT Conference (American Statistical Association (ASA) in Statistics and Data Science)",
-        "excerpt":"UPSTAT Conference is a regional conference of American Statistical Association (ASA) in Statistics and Data Science. Our students attend and present their capstone projects in Probability, Statistics, Machine Learning and Stochastic Modeling. In 2018, it was held at University of Rochester on April 20-21. Our Geneseo math students contributed to...","categories": ["DataFest"],
-        "tags": [],
-        "url": "bilgicyusuf.github.io/datafest/2018/04/20/datafest.html"
-      }]
